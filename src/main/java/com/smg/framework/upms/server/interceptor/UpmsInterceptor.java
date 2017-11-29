0eddd665b5ccc9dc2fa9dc95dccde4ca8624591d
Justin Cai <13119725@qq.com>
2017-11-29 16:39:06 +0800

    优化FatherToChildUtil.java

--- src/main/java/com/smg/framework/upms/server/interceptor/UpmsInterceptor.java
+++ src/main/java/com/smg/framework/upms/server/interceptor/UpmsInterceptor.java
@@ -9,7 +9,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
